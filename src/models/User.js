7b04117e90537abd7bd5a5af3fc194a1a5e873fe
andriy.nikiforov <andriy.nikiforov.general@gmail.com>
2021-02-11 11:53:33 +0200

    Fix model User.

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,4 @@
 const { sequelize: Sequelize, DataTypes, Model } = require('../../config/db');
-const { sequelize } = require('./Token');
 
 class User extends Model { }
 
@@ -8,11 +7,6 @@ User.init({
   password: DataTypes.STRING,
   email: DataTypes.STRING,
   bio: DataTypes.TEXT,
-  tags: DataTypes.TSVECTOR,
-  team_id: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-  },
   additional_info: DataTypes.JSON,
 }, {
   sequelize: Sequelize,
@@ -20,18 +14,6 @@ User.init({
   tableName: 'users',
   createdAt: 'created_at',
   updatedAt: 'updated_at',
-});
-
-User.beforeCreate(async (user) => {
-  const { tags } = user;
-
-  if (tags) {
-    const readyData = tags.map((item) => `'${item}'`)
-      .filter(Boolean)
-      .join(" || ' ' || ");
-
-    user.tags = sequelize.query(`to_tsvector(${readyData})`);
-  }
 });
 
 module.exports = User;
